d5d76baaaadfe5ccc40d5393dcb016a8980b2c03
Yiğit Eren <yigit.eren.tri@gmail.com>
2020-02-18 19:02:17 +0300

    Created holes
    Created some hole mechanics. Still needs improvement.

---
+++ Assets/Scripts/Hole/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<HoleSpawn> holeSpawns = new List<HoleSpawn>();
+    [SerializeField] private GameObject holePrefab;
+
+    private bool isStarted = false;
+
+    void Update()
+    {
+        if(!isStarted)
+        {
+            isStarted = true;
+            StartCoroutine(SpawnAHole());
+        }
+    }
+
+    IEnumerator SpawnAHole()
+    {
+        yield return new WaitForSeconds(3f);
+
+        HoleSpawn randomSpawn = holeSpawns[Random.Range(0, holeSpawns.Count - 1)];
+
+        Instantiate(holePrefab, randomSpawn.pos, randomSpawn.rot);
+        isStarted = false;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Hole/HoleSpawnPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleSpawnPosition : MonoBehaviour
+{
+    void Awake()
+    {
+
+        HoleSpawn holeSpawn = new HoleSpawn();
+        holeSpawn.pos = transform.position;
+        holeSpawn.rot = transform.rotation;
+
+        GameObject gameManager = GameObject.Find("Game Manager");
+        gameManager.GetComponent<GameManager>().holeSpawns.Add(holeSpawn);
+
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        renderer.enabled = false;
+    }
+}
+
+public struct HoleSpawn
+{
+    public Vector3 pos;
+    public Quaternion rot;
+}

16da333b47a64cf82bf2633a995e223d098b1bc8
Fahrican Elidemir <fahricanelidemir@gmail.com>
2020-02-21 01:47:19 +0300

    Functional Pumps
    Created a Pump script to make pipes functional.

---
+++ Assets/Scripts/Other/Pump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pump : MonoBehaviour
+{
+    GameManager gameManager;
+    [SerializeField] private float pumpPower = 2f;
+    [SerializeField] private float pumpDrawHeight = 0f;
+    private float decrease;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        decrease = gameManager.GetHoleFillAmount() * pumpPower;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameManager.DecreaseWaterLevel(decrease * Time.deltaTime);
+        if(gameManager.GetCurrentWaterAmount() < pumpDrawHeight)
+        gameManager.SetCurrentWaterAmount(pumpDrawHeight);
+        
+        Debug.Log(gameManager.GetCurrentWaterAmount());
+    }
+}

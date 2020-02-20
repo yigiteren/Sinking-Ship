472d42fca9c3a47898755c2bfa8c8bd7d15aa757
Yiğit Eren <yigit.eren.tri@gmail.com>
2020-02-20 12:12:44 +0300

    Added Water Indicator
    Added water indicator to show that the ship is getting full of water.

--- Assets/Scripts/Hole/GameManager.cs
+++ Assets/Scripts/Hole/GameManager.cs
@@ -7,19 +7,54 @@ public class GameManager : MonoBehaviour
 
     public List<HoleSpawn> holeSpawns = new List<HoleSpawn>();
     [SerializeField] private GameObject holePrefab = null;
+    [SerializeField] private GameObject waterLevelObject = null;
     [SerializeField] private float holeSpawnFrequency = 3f;
+    [SerializeField] private float holeFillAmountPerSecond = 2f;
+    [SerializeField] private float maxWaterAmount = 100f;
 
+    private float currentWaterAmount = 0;
     private bool isStarted = false;
     private int currentHoleSpawnID = 0;
 
+    private float waterLevelStartingHeight = 0;
+    [SerializeField] private float waterLevelFinalHeight = 1.5f;
+
+
+    private void Start()
+    {
+        waterLevelStartingHeight = waterLevelObject.transform.position.y;
+        waterLevelFinalHeight += waterLevelStartingHeight;
+    }
+
     void Update()
     {
+        UpdateWaterLevel();
         //Instantiates holes for test purposes.
         if(!isStarted)
         {
             isStarted = true;
             StartCoroutine(SpawnAHole());
         }
+    }
+
+    private void UpdateWaterLevel()
+    {
+        foreach(HoleSpawn spawn in holeSpawns)
+        {
+            if(spawn.canSpawn == false) // This means it is full.
+            {
+                currentWaterAmount += holeFillAmountPerSecond * Time.deltaTime;
+                if (currentWaterAmount > maxWaterAmount) currentWaterAmount = maxWaterAmount;
+            }
+        }
+
+        Debug.Log("Current water percentage: " + GetWaterPercentage() + "%");
+
+        float heightOfWater = Map(0, maxWaterAmount, waterLevelStartingHeight, waterLevelFinalHeight, currentWaterAmount);
+        Vector3 newPos = waterLevelObject.transform.position;
+        newPos.y = heightOfWater;
+        waterLevelObject.transform.position = newPos;
+
     }
 
     public void AddNewHoleSpawnToList(HoleSpawn holeSpawn)
@@ -55,5 +90,8 @@ public class GameManager : MonoBehaviour
         holeSpawns[holeToSetFree.id] = holeToSetFree;
 
     }
+
+    public float GetWaterPercentage() { return currentWaterAmount / maxWaterAmount * 100; }
+    static float Map(float a1, float a2, float b1, float b2, float s) => b1 + (s - a1) * (b2 - b1) / (a2 - a1);
 
 }

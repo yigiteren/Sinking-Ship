de78a132bc4735529d32bf2fff0b843f3953f249
Yiğit Eren <yigit.eren.tri@gmail.com>
2020-02-22 19:35:16 +0300

    Final changes and build
    Removed a bug where player could walk faster and added some more speed to player.

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -26,8 +26,17 @@ public class MovementController : MonoBehaviour
         float verticalMovement = movementSpeed * Input.GetAxis("Vertical");
 
         Vector3 motion = new Vector3(horizontalMovement, 0, verticalMovement);
+
         motion = transform.TransformVector(motion);
 
+        if (motion.x > movementSpeed) motion.x = movementSpeed;
+        if (motion.x < -movementSpeed) motion.x = -movementSpeed;
+
+        if (motion.z > movementSpeed) motion.z = movementSpeed;
+        if (motion.z < -movementSpeed) motion.z = -movementSpeed;
+
         controller.Move(motion * Time.deltaTime);
+
+        Debug.Log(controller.velocity);
     }
 }
